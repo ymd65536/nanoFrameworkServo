2db9b00ad4c19f87d0596eae8d78955d9340cca5
ymd65536 <ymd65536@gmail.com>
2026-05-03 23:56:15 +0900

    Fire初期化・画面クリア処理を削除し出力のみ実行
    Fireの初期化、画面クリア、表示前の待機処理を削除し、"Hello Fire!" の出力のみを行うように変更しました。無限待機は維持しています。

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
 ﻿using nanoFramework.M5Stack; // Fireクラスに必要
 using System.Threading;
 
-// 1. 初期化
 Fire.InitializeScreen();
-
-// 3. Clearを呼んで画面全体をBackgroundColorで塗りつぶす
 nanoFramework.M5Stack.Console.Clear();
-
-// 4. 少し待ってから表示
-Thread.Sleep(500);
 nanoFramework.M5Stack.Console.WriteLine("Hello Fire!");
 
 Thread.Sleep(Timeout.Infinite);
